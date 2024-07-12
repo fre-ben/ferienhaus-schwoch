6cb5ce1ad5df56c80db5ff36b89eff9ee289be8d
frederik.schocke <frederik.schocke@aoe.com>
2024-07-12 08:44:22 +0200

    fix: change wording from wohnung to haus

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-  title: `Böllenhäuschen | Ferienwohnung in Hollenstedt`,
-  description: `Genießen Sie Ihren Urlaub im Böllenhäuschen, unserer gemütlichen Ferienwohnung in Hollenstedt. Entspannen Sie im Leinetal und erleben Sie erholsame Tage fernab vom Alltagsstress.`,
+  title: `Böllenhäuschen | Ferienhaus in Hollenstedt`,
+  description: `Genießen Sie Ihren Urlaub im Böllenhäuschen, unserem gemütlichen Ferienhaus in Hollenstedt. Entspannen Sie im Leinetal und erleben Sie erholsame Tage fernab vom Alltagsstress.`,
   url: `https://www.boellenhaeuschen.de`, // No trailing slash!
   name: `Böllenhäuschen`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
